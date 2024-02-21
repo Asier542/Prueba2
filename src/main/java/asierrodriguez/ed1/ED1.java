2c499ef7fda3753d669bb386cb3467f2f6f6b5ff
Asier542 <asierrodriguezormaechea@gmail.com>
2024-02-21 15:35:08 +0100

    prueba

---
+++ src/main/java/asierrodriguez/ed1/ED1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asierrodriguez.ed1;
+
+/**
+ *
+ * @author DAM105
+ */
+public class ED1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
